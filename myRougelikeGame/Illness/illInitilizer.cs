8ed2804ff6fa7913dd58118764e978b0a5f7da7a
DoyouhaveJJ <934762331@qq.com>
2019-04-18 10:25:23 +0800

    none

--- myRougelikeGame/Illness/illInitilizer.cs
+++ myRougelikeGame/Illness/illInitilizer.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using myRougelikeGame.Function;
+using myRougelikeGame.Illness.ills;
 namespace myRougelikeGame.Illness
 {
     class illInitilizer
     {
-        
-        private DIYRandom dr = new DIYRandom();
 
+
+
+        public defaultIll getIllByID(int ID)
+        {
+            switch (ID)
+            {
+                case 1: { return new ill_Cold(); }
+                default: return null;
+            
+            }
+
+        }
 
         
 
--- myRougelikeGame/Illness/illSystem.cs
+++ myRougelikeGame/Illness/illSystem.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using myRougelikeGame.Player;
+using myRougelikeGame.Illness.ills;
+using myRougelikeGame.Function;
 namespace myRougelikeGame.Illness
 {
     class illSystem
     {
         private theHero myhero;
         private illInitilizer iii = new illInitilizer();
+        private defaultIll ill_Hero;
+        private DIYRandom dr = new DIYRandom();
+
+        public defaultIll InitIlllByIDRate(int ID, int rate)
+        {
+            if (dr.startBet(rate, 1000))
+            {
+                return iii.getIllByID(ID);
+            }
+            else
+            {
+                return null;
+            }
+        }
 
 
+        public void HeroGetIll(defaultIll ill){
+            ill_Hero = ill;
+        }
+
+        public void SufferByTurn(){
 
 
+        }
+
+        public void TryCureBySelf()
+        {
 
 
+        }
+        public void Cure()
+        {
+
+
+        }
 
 
 
